3adc6a46555d923764027405f0dbd6d53bb4aa02
Hogwon Kim <hogwonkim@Hogwonui-MacBookAir.local>
2024-03-01 17:36:01 +0900

    profile page complete#4

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,4 +1,4 @@
-import { addClassNames } from "@/libs/client/utils";
+import { addClassNames } from "@/libs/utils";
 
 interface ButtonProps {
   large?: boolean;
@@ -16,7 +16,7 @@ export default function Button({
     <button
       {...rest}
       className={addClassNames(
-        "w-full bg-green-500 hover:bg-green-600 text-white  px-4 border border-transparent rounded-md shadow-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-green-500 focus:outline-none",
+        "w-full bg-green-500 hover:bg-green-600 text-white  px-4 border border-transparent rounded-2xl shadow-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-green-500 focus:outline-none",
         large ? "py-3 text-base" : "py-2 text-sm "
       )}
     >
