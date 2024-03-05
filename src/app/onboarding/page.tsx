a279f13f28ff733674583f2fdbd6f59b7ab24004
eunsiho <versailles@korea.ac.kr>
2024-03-05 13:49:31 +0900

    create tsconfig.paths.json#6

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 
+import Layout from "@app/components/layout";
 import { error } from "console";
 import { useForm } from "react-hook-form";
-import Layout from "../components/layout";
-import Button from "../components/button";
-import Input from "../components/input";
 
 interface IForm {
   nickname: string;
