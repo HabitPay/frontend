3ebbdb64fc3c5e058166dc97e202be414847f9ab
eunsiho <versailles@korea.ac.kr>
2024-03-05 15:19:47 +0900

    add post request to backend #8

--- src/app/components/googleCustomButton.tsx
+++ src/app/components/googleCustomButton.tsx
@@ -2,8 +2,18 @@ import { useGoogleLogin } from "@react-oauth/google";
 import CustomButton from "./customButton";
 
 const GoogleCustomButton = () => {
+  const backend = "https://localhost:3001";
   const login = useGoogleLogin({
-    onSuccess: (res) => console.log(res),
+    onSuccess: (res) => {
+      console.log(res.code);
+      fetch(backend, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(res.code),
+      });
+    },
     onError: () => console.log("Login Error"),
     flow: "auth-code",
   });
