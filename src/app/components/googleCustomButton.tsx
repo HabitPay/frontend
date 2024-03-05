da99978633327636a3a9978bfaa10dbb4f5201e7
eunsiho <versailles@korea.ac.kr>
2024-03-05 20:29:27 +0900

    test

--- src/app/components/googleCustomButton.tsx
+++ src/app/components/googleCustomButton.tsx
@@ -6,16 +6,18 @@ const GoogleCustomButton = () => {
   const login = useGoogleLogin({
     onSuccess: (res) => {
       console.log(res.code);
-      fetch(backend, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(res.code),
-      });
+      // fetch(backend, {
+      //   method: "POST",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      //   body: JSON.stringify(res.code),
+      // });
     },
     onError: () => console.log("Login Error"),
     flow: "auth-code",
+    ux_mode: "redirect",
+    redirect_uri: "http://localhost:3000/google_redirect",
   });
 
   return (
