6b4c19fa93fc38b509e4cfa044c939e9f901e0d1
eunsiho <versailles@korea.ac.kr>
2024-03-05 20:29:27 +0900

    test

---
+++ src/app/google_redirect/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Page = () => {
+  return (
+    <div className=" bg-inherit h-screen flex flex-col justify-center items-center  space-y-8">
+      <div className="flex flex-col items-center">로그인 중입니다.</div>
+    </div>
+  );
+};
+
+export default Page;
