3b9df00ac12fdeb87aa15e1cab99372fa9f5a880
eunsiho <versailles@korea.ac.kr>
2024-03-05 15:19:47 +0900

    add post request to backend #8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import GoogleCustomButton from "./components/googleCustomButton";
 
 const Page = () => {
-  const clientId = "abc";
-
+  const clientId =
+    "957321976468-mreg0vucneh2frl7hkq90ki7c60htm6q.apps.googleusercontent.com";
   return (
-    <GoogleOAuthProvider clientId={clientId}>
+    <GoogleOAuthProvider clientId={clientId ? clientId : "error"}>
       <div className=" bg-inherit h-screen flex flex-col justify-center items-center  space-y-8">
         <div className="flex flex-col items-center">
           <span className=" font-light">성공하는 습관 만들기</span>
