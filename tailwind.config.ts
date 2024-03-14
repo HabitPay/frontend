8b727b55b26da8d5da44b2870016df28fc1380e3
eunsiho <versailles@korea.ac.kr>
2024-03-14 18:51:24 +0900

    figma에 있는 색상을 추출해서 tailwind.confing.ts에 추가#10

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,16 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {},
+  content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        "habit-green": "#66BB6A",
+        "habit-gray": "#AAAAAA",
+        "habit-background": "#EEEEEE",
+      },
+    },
+  },
   darkMode: "media",
   plugins: [],
 };
