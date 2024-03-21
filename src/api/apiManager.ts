fa8bf2a59bcab7b86b99912796902eb2faf0a679
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-03-21 22:47:37 +0900

    ✨ feat: Add Authorization header field in axios instance #16

--- src/api/apiManager.ts
+++ src/api/apiManager.ts
@@ -1,8 +1,13 @@
-import axios, {AxiosInstance} from 'axios';
+"use client";
+
+import axios, { AxiosInstance } from "axios";
 
 const apiManager: AxiosInstance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_BACKEND_SERVER}`,
   timeout: 3000,
+  headers: {
+    Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
+  },
 });
 
 export default apiManager;
