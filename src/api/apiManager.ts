8b9e553e13834636e941aebf89848199e1298a37
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-03-21 22:53:43 +0900

    ✨ feat: Fix getting sessionStorage from server side #16
    - 자세한 원리는 모르겠지만, 서버 사이드에서는 sessionStorage 에 접근할 수 없다고 함.
    - 서버 사이드에서는 window 가 undefined 로 설정되는 것 같음.

--- src/api/apiManager.ts
+++ src/api/apiManager.ts
@@ -1,12 +1,18 @@
 "use client";
 
 import axios, { AxiosInstance } from "axios";
+const getAccessToken = () => {
+  if (typeof window !== "undefined") {
+    return sessionStorage.getItem("accessToken");
+  }
+  return null;
+};
 
 const apiManager: AxiosInstance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_BACKEND_SERVER}`,
   timeout: 3000,
   headers: {
-    Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
+    Authorization: `Bearer ${getAccessToken()}`,
   },
 });
 
