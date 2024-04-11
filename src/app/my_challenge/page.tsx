d16d22f8df148c47b8efd0dda8d93072a24ce20e
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-04-11 13:11:35 +0900

    create floating button#15

--- src/app/my_challenge/page.tsx
+++ src/app/my_challenge/page.tsx
@@ -12,6 +12,7 @@ import Challenges, {
   IChallenges,
 } from "./components/challenge";
 import profilePic from "../../../public/profilePic.jpeg";
+import FloatingButton from "@app/components/floatingButton";
 
 const inProgressChallenge: IChallengeInfo[] = [
   {
@@ -118,6 +119,25 @@ const Page = () => {
           />
         </div>
         <Challenges challenges={challenges} challengeState={challengesButton} />
+        <FloatingButton href="/create_challenge">
+          <div className="flex items-center justify-center text-sm">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-4 h-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg>
+            <span>챌린지 생성</span>
+          </div>
+        </FloatingButton>
       </div>
     </Layout>
   );
