779894e2eb27234e91c0202177a255164fd1f6e0
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-05-22 17:07:36 +0900

    ✨ feat: 챌린지 제목, 남은 일수 계산 표시 #34

--- src/app/challenges/[id]/components/challengeTitle.tsx
+++ src/app/challenges/[id]/components/challengeTitle.tsx
@@ -1,13 +1,25 @@
 import Image from "next/image";
 import profilePic from "../../../../../public/profilePic.jpeg";
 
-const ChallengeTitle = () => {
+interface IChallengeTitleProps {
+  title: string;
+  remainingDays: number;
+  participants: number;
+  profileImages: string | null;
+}
+
+const ChallengeTitle = ({
+  title,
+  remainingDays,
+  participants,
+  profileImages,
+}: IChallengeTitleProps) => {
   return (
     <div className="flex items-center justify-between mt-4">
       <div className="flex flex-col">
-        <div className="text-2xl font-bold">책업일치</div>
+        <div className="text-2xl font-bold">{title}</div>
         <div className="text-xl font-[250] text-habit-gray">
-          종료까지 18일 남았습니다.
+          종료까지 {remainingDays}일 남았습니다.
         </div>
       </div>
       <div className="flex items-center px-3 space-x-2 border-2 h-11 rounded-2xl border-habit-gray">
@@ -15,20 +27,20 @@ const ChallengeTitle = () => {
           <Image
             src={profilePic}
             className="z-30 rounded-full size-9 "
-            alt="profilePicture of atendees"
+            alt="profilePicture of attendees"
           />
           <Image
             src={profilePic}
             className="z-20 rounded-full size-9"
-            alt="profilePicture of atendees"
+            alt="profilePicture of attendees"
           />
           <Image
             src={profilePic}
             className="z-10 rounded-full size-9 "
-            alt="profilePicture of atendees"
+            alt="profilePicture of attendees"
           />
         </div>
-        <div>42</div>
+        <div>{participants}</div>
       </div>
     </div>
   );
