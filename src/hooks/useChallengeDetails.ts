0309ab6132efae098b71de036f9d7f7fe70b1a44
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-05-22 23:10:28 +0900

    ✨ feat: useChallengeDetails 커스텀 훅 반환 값으로 요일 배열 추가 #36
    - 비트 연산 적용해서 선택된 날짜 배열을 반환

--- src/hooks/useChallengeDetails.ts
+++ src/hooks/useChallengeDetails.ts
@@ -2,18 +2,34 @@ import { useState, useEffect } from "react";
 
 import apiManager from "@api/apiManager";
 import { IChallengeDetailsDto } from "@/types/challenge";
+import { SelectedStatus } from "@/types/enums";
+
+const calculateSelectedDays = (selectedDays: number[], days: number) => {
+  selectedDays.forEach((_, idx) => {
+    if (days & (1 << (6 - idx))) {
+      selectedDays[idx] = SelectedStatus.SELECTED;
+    }
+  });
+  return selectedDays;
+};
 
 export const useChallengeDetails = (id: string) => {
   const [challengeDetails, setChallengeDetails] =
     useState<IChallengeDetailsDto | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
+  const [selectedDays, setSelectedDays] = useState<number[]>(
+    new Array(7).fill(SelectedStatus.NOT_SELECTED)
+  );
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const res = await apiManager(`/challenge/${id}`);
         setChallengeDetails(res.data);
+        setSelectedDays(
+          calculateSelectedDays(selectedDays, res.data.participatingDays)
+        );
       } catch (err) {
         setError(err as Error);
       } finally {
@@ -24,5 +40,5 @@ export const useChallengeDetails = (id: string) => {
     fetchData();
   }, [id]);
 
-  return { challengeDetails, isLoading, error };
+  return { challengeDetails, selectedDays, isLoading, error };
 };
