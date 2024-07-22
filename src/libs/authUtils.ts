540fc81698d6cec6ba8bb2d001c1cf490af81b52
eunsiho <versailles@korea.ac.kr>
2024-07-23 01:35:37 +0900

    verifyAccessToken에 추가 필요한 작업 주석 추가 #57

--- src/libs/authUtils.ts
+++ src/libs/authUtils.ts
@@ -3,6 +3,8 @@
 import { getAccessToken } from "@/api/apiManager";
 import { redirect } from "next/navigation";
 
+// async함수가 되어야함.
+// 백엔드에 accessToken을 보내고 유효기간 검증 과정 필요
 export function verifyAccessToken() {
   const accessToken = getAccessToken();
   if (accessToken) {
