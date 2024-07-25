7e4239d8ccbeb5219d458ccb7756be87f7f21613
joonhan <joonhan@student.42seoul.kr>
2024-07-25 14:46:44 +0900

    ✨ feat: 챌ㄹ니지 상태 별로 해당하는 항목이 없을 때 표시 추가 #58

--- src/app/challenges/my_challenge/components/challenges.tsx
+++ src/app/challenges/my_challenge/components/challenges.tsx
@@ -2,43 +2,48 @@ import ChallengeCard from "./challengeCard";
 import { ChallengeStatesEnum } from "@/types/enums";
 import { IChallengeEnrolledList } from "@/types/challenge";
 
-export interface IChallengeInfo {
-  title: string;
-  participation: boolean;
-  participants: number;
-  startAt: Date;
-  endAt: Date;
-  fee: number;
-  achievement: number;
-}
-
 interface IChallengesProps {
   challenges: IChallengeEnrolledList;
   challengeState: ChallengeStatesEnum;
 }
 
 function Challenges({ challenges, challengeState }: IChallengesProps) {
-  if (challenges.inProgress.length === 0) {
-    return (
-      <span>참여 중인 챌린지가 없습니다. 새로운 챌린지에 참여해보세요!</span>
-    );
-  }
-
   return (
     <>
-      {challengeState == ChallengeStatesEnum.InProgress && (
-        <ChallengeCard
-          items={challenges.inProgress}
-          challengeState={challengeState}
-        />
-      )}
+      {challengeState == ChallengeStatesEnum.InProgress &&
+        challenges.inProgress.length === 0 && (
+          <span>
+            참여 중인 챌린지가 없습니다. 새로운 챌린지에 참여해보세요!
+          </span>
+        )}
 
-      {challengeState == ChallengeStatesEnum.Scheduled && (
-        <ChallengeCard
-          items={challenges.scheduled}
-          challengeState={challengeState}
-        />
-      )}
+      {challengeState == ChallengeStatesEnum.InProgress &&
+        challenges.inProgress.length > 0 && (
+          <ChallengeCard
+            items={challenges.inProgress}
+            challengeState={challengeState}
+          />
+        )}
+
+      {challengeState == ChallengeStatesEnum.Scheduled &&
+        challenges.scheduled.length === 0 && (
+          <span>
+            진행 예정인 챌린지가 없습니다. 새로운 챌린지에 참여해보세요!
+          </span>
+        )}
+
+      {challengeState == ChallengeStatesEnum.Scheduled &&
+        challenges.scheduled.length > 0 && (
+          <ChallengeCard
+            items={challenges.scheduled}
+            challengeState={challengeState}
+          />
+        )}
+
+      {challengeState == ChallengeStatesEnum.Completed &&
+        challenges.completed.length === 0 && (
+          <span>종료된 챌린지가 없습니다.</span>
+        )}
 
       {challengeState == ChallengeStatesEnum.Completed && (
         <ChallengeCard
