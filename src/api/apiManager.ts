807b4d2559efe769f2c56e7ca7dc2ad54a2c41a5
Hogwon Kim <404yonara.elegy@gmail.com>
2024-07-26 17:04:13 +0900

    create/refresh token interceptor#57

--- src/api/apiManager.ts
+++ src/api/apiManager.ts
@@ -1,6 +1,13 @@
 "use client";
 
-import axios, { AxiosInstance } from "axios";
+import { removeJwtFromSessionStorage } from "@/libs/jwt";
+import axios, {
+  AxiosError,
+  AxiosInstance,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from "axios";
+import { useRouter } from "next/navigation";
 
 export const getAccessToken = () => {
   if (typeof window !== "undefined") {
@@ -35,6 +42,53 @@ apiManager.interceptors.request.use(
   },
   (error) => {
     console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+apiManager.interceptors.response.use(
+  async function (response: AxiosResponse): Promise<AxiosResponse> {
+    return response;
+  },
+  async function (error: AxiosError): Promise<AxiosResponse | Promise<never>> {
+    const router = useRouter();
+    const logout = () => {
+      removeJwtFromSessionStorage();
+      router.push("/");
+    };
+
+    const { config, response } = error;
+    const originalRequest = config as InternalAxiosRequestConfig;
+
+    if (!response) {
+      return Promise.reject(error);
+    }
+
+    const { status, data } = response;
+
+    if (status === 401) {
+      if (data === "InvalidTokenException") {
+        logout();
+      } else if (data === "TokenExpired") {
+        try {
+          const tokenRefreshResult: AxiosResponse = await apiManager.post(
+            "/token"
+          );
+          if (tokenRefreshResult.status === 200) {
+            const { accessToken } = tokenRefreshResult.data;
+            sessionStorage.setItem("accessToken", accessToken);
+            originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+            return apiManager(originalRequest);
+          } else {
+            logout();
+          }
+        } catch (e) {
+          console.log(e);
+          logout();
+        }
+      }
+    }
+
     return Promise.reject(error);
   }
 );
