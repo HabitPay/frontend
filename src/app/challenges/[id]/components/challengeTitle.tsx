a7c5f614e1e6bb9f060df007690fc984d62b4529
joonhan <joonhan@student.42seoul.kr>
2024-07-28 19:54:18 +0900

    ✨ feat: 챌린지 참여자 프로필 이미지 클래스 이름 동적 생성 #62

--- src/app/challenges/[id]/components/challengeTitle.tsx
+++ src/app/challenges/[id]/components/challengeTitle.tsx
@@ -34,11 +34,12 @@ const ChallengeTitle = ({
       </div>
       <div className="flex items-center px-3 space-x-2 border-2 h-11 rounded-2xl border-habit-gray">
         <div className="flex -space-x-2">
-          {profileImages.map((profileImage) => {
+          {profileImages.map((profileImage, index) => {
+            const className=`z-${30 - index * 10}30 rounded-full size-9`;
             return (
               <Image
                 src={profileImage || defaultProfileImage}
-                className="z-30 rounded-full size-9 "
+                className={className}
                 alt="profile image"
                 width={36}
                 height={36}
