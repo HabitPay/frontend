c56b2ffae87c1d985d5268e6309bd344832eabe9
Hogwon Kim <404yonara.elegy@gmail.com>
2024-07-29 15:46:02 +0900

    withAuth에 ToastPopup 적용#57

--- src/app/components/withAuth.tsx
+++ src/app/components/withAuth.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { toastPopupAtom } from "@/hooks/atoms";
 import { NextPage } from "next";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { useSetRecoilState } from "recoil";
 
 const withAuth = <P extends object>(
   WrappedComponent: NextPage<P>
@@ -10,17 +12,25 @@ const withAuth = <P extends object>(
   const ComponentWithAuth: NextPage<P> = (props) => {
     const router = useRouter();
     const [loading, setLoading] = useState(true);
+    const setToastPopup = useSetRecoilState(toastPopupAtom);
 
     useEffect(() => {
       const accessToken = sessionStorage.getItem("accessToken");
 
       if (!accessToken) {
-        // 스낵바
-        router.push("/");
+        setToastPopup({
+          message: "다시 로그인 해 주세요",
+          top: false,
+          success: false,
+        });
+        const timer = setTimeout(() => {
+          router.push("/");
+        }, 2700);
+        return () => clearTimeout(timer);
       } else {
         setLoading(false);
       }
-    }, [router]);
+    }, [router, setToastPopup]);
 
     if (loading) {
       return <p>Loading...</p>;
