6c6d092c42a188c09c333bade63a565e7b7276bc
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-08-26 22:54:29 +0900

    👷 chore: autodeploy runner 옵션 제거

--- sst.config.ts
+++ sst.config.ts
@@ -18,7 +18,6 @@ export default $config({
         ) {
           return {
             stage: "production",
-            runner: { engine: "codebuild", compute: "large" },
           };
         }
       },
