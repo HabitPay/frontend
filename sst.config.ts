5bd71d278fe3cfa9c4b98090e186bb71594fe260
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-08-26 20:52:44 +0900

    👷 chore: main 브랜치에서 autodeploy 하도록 설정 #76

--- sst.config.ts
+++ sst.config.ts
@@ -8,6 +8,22 @@ export default $config({
       home: "aws",
     };
   },
+  console: {
+    autodeploy: {
+      target(event) {
+        if (
+          event.type === "branch" &&
+          event.branch === "main" &&
+          event.action === "pushed"
+        ) {
+          return {
+            stage: "production",
+            runner: { engine: "codebuild", compute: "large" },
+          };
+        }
+      },
+    },
+  },
   async run() {
     new sst.aws.Nextjs("Habitpayfrontend", {
       domain: "habitpay.link",
