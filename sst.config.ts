c25b68e779009de88b9e859d99e3af684f386af6
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-09-22 17:10:14 +0900

    chore: region 환경변수 제거 #84

--- sst.config.ts
+++ sst.config.ts
@@ -8,7 +8,7 @@ export default $config({
       home: "aws",
       providers: {
         aws: {
-          region: process.env.AWS_REGION,
+          region: "ap-northeast-2",
         },
       },
     };
