0a5a69dfd20602fcdb5083ee48f64fada35649ad
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-09-24 10:00:09 +0900

    🐛 fix: DTO 이름 수정 #93

--- src/hooks/useMemberProfile.ts
+++ src/hooks/useMemberProfile.ts
@@ -1,13 +1,10 @@
 import { useState, useEffect } from "react";
 
-import {
-  IMemberProfileDto
-} from "@/types/member";
+import { IProfileDTO } from "@/types/member";
 import { fetchMemberProfile } from "@/api/member";
 
 export const useMemberProfile = () => {
-  const [memberProfile, setMemberProfile] =
-    useState<IMemberProfileDto | null>(null);
+  const [memberProfile, setMemberProfile] = useState<IProfileDTO | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
 
