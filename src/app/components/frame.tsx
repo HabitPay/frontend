f3011f1f3b3f9d06e76e6b6a569792d6f5a088c0
eunsiho <versailles@korea.ac.kr>
2024-09-24 20:41:14 +0900

    feat/challege-list페이지 생성#101

--- src/app/components/frame.tsx
+++ src/app/components/frame.tsx
@@ -98,7 +98,7 @@ const Frame = ({
               </svg>
             </div>
           </Link>
-          <Link href="/notice">
+          <Link href="/challenges/challenge-list">
             <div
               className={addClassNames(
                 "flex flex-col items-center space-y-2 p-3",
@@ -118,7 +118,7 @@ const Frame = ({
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
+                  d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
                 />
               </svg>
             </div>
