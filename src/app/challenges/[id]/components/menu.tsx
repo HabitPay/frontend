89eadda437f18b14add5fea91163b7bcb8e4933f
eunsiho <versailles@korea.ac.kr>
2024-10-11 09:37:45 +0900

    다시 z-index수정, popup을 기본 bottom으로 오게 수정#93

--- src/app/challenges/[id]/components/menu.tsx
+++ src/app/challenges/[id]/components/menu.tsx
@@ -9,7 +9,7 @@ export interface IMenuProps {
 
 const Menu = ({ challengeId, currentPage }: IMenuProps) => {
   return (
-    <div className="flex pb-3 space-x-5 text-xl border-b-2 border-habit-lightgray z-[60]">
+    <div className="flex pb-3 space-x-5 text-xl border-b-2 border-habit-lightgray">
       <Link href={`/challenges/${challengeId}/main`}>
         <span className={currentPage === "참여 기록" ? "text-habit-gray" : ""}>
           챌린지 메인
