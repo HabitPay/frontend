1599e0671421c9d182a04e5b6cf9a6b4ccd271a1
JoonHyeok Han <joonhyeok.han@gmail.com>
2024-12-19 23:22:24 +0900

    refactor: enum에서 객체로 변경 #136
    - 상수를 불러와서 동적으로 메세지를 생성하기 위함

--- src/types/enums/popupMessage.ts
+++ src/types/enums/popupMessage.ts
@@ -1,5 +1,15 @@
-export enum PopupErrorMessage {
-  ReLoginRequired = "다시 로그인 해 주세요",
-  UnsupportedFileType = "지원되는 파일 형식은 JPEG, JPG, PNG, GIF입니다.",
-  FileSizeExceeded = "파일 크기는 1MB를 초과할 수 없습니다.",
-}
+import {
+  MAX_FILE_SIZE,
+  MB,
+  SUPPORTED_IMAGE_EXTENSIONS,
+} from "@/libs/constants";
+
+const supportedFileTypes = SUPPORTED_IMAGE_EXTENSIONS.map(
+  (type) => type.split("/")[1]
+).join(", ");
+
+export const PopupErrorMessage = {
+  ReLoginRequired: "다시 로그인 해 주세요",
+  UnsupportedFileType: `지원되는 파일 형식은 ${supportedFileTypes} 입니다.`,
+  FileSizeExceeded: `파일 크기는 ${MAX_FILE_SIZE / MB}MB를 초과할 수 없습니다.`,
+} as const;
