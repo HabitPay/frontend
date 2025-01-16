c531087f57a332d5f82d453c3519a41d1e92b7b8
eunsiho <versailles@korea.ac.kr>
2025-01-17 06:08:27 +0900

    수정완료#175

--- src/app/challenges/[challengeId]/components/enrollment.tsx
+++ src/app/challenges/[challengeId]/components/enrollment.tsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { useSetRecoilState } from "recoil";
 
 import apiManager from "@/api/apiManager";
 import Button from "@/app/components/button";
 import { toastPopupAtom } from "@/hooks/atoms";
+import { useRouter } from "next/navigation";
+import { getId } from "@/libs/jwt";
 
 interface IEnrollmentProps {
   id: string;
@@ -17,6 +19,9 @@ const Enrollment = ({
   isMemberEnrolledInChallenge,
   isHost,
 }: IEnrollmentProps) => {
+  const router = useRouter();
+  const [myId, setMyId] = useState<string | null | undefined>();
+
   const [
     isMemberEnrolledInChallengeState,
     setIsMemberEnrolledInChallengeState,
@@ -26,13 +31,32 @@ const Enrollment = ({
   const enrollChallenge = async () => {
     try {
       const res = await apiManager.post(`/challenges/${id}/enroll`);
-
       setIsMemberEnrolledInChallengeState(true);
       setToastPopup({
         message: res.data.message,
         top: false,
         success: true,
       });
+    } catch (error) {
+      setToastPopup({
+        // @ts-ignore
+        message: error.data.message,
+        top: false,
+        success: false,
+      });
+    }
+  };
+
+  const deleteChallenge = async () => {
+    try {
+      const res = await apiManager.delete(`/challenges/${id}`);
+      setToastPopup({
+        message: res.data.message,
+        top: false,
+        success: true,
+      });
+      setIsMemberEnrolledInChallengeState(false);
+      router.push(`/${myId}/challenge`);
     } catch (error) {
       setToastPopup({
         // @ts-ignore
@@ -52,6 +76,7 @@ const Enrollment = ({
         top: false,
         success: true,
       });
+      router.push(`/${myId}/challenge`);
     } catch (error) {
       setToastPopup({
         // @ts-ignore
@@ -62,8 +87,15 @@ const Enrollment = ({
     }
   };
 
+  useEffect(() => {
+    setMyId(getId());
+  }, []);
   if (isHost) {
-    return <></>;
+    return (
+      <>
+        <Button color="red" onClick={deleteChallenge} text="챌린지 삭제" />
+      </>
+    );
   }
 
   return (
